4b1f766b42a5775a836c917ba2d30a0bc8a0c9c0
FraGit69 <francesco.re@itiscuneo.eu>
2024-09-20 13:09:10 +0200

    socket and client finally working

--- client.py
+++ client.py
@@ -9,4 +9,5 @@ server_address = ("192.168.65.19", 6980)
 BUFFER_SIZE = 4092 #uanti bit posso inviare o ricevere
 
 udp_client_socket.sendto(message, server_address)
-
+data, server_address = udp_client_socket.recvfrom(BUFFER_SIZE)
+print(f"Messaggio ricevuto da server: {data.decode('utf-8')} da {server_address}")
--- server.py
+++ server.py
@@ -6,5 +6,8 @@ BUFFER_SIZE = 4092 #uanti bit posso inviare o ricevere
 udp_server_socket = s.socket(s.AF_INET, s.SOCK_DGRAM) #dgram è per udp stream è per tcp
 udp_server_socket.bind(server_address)
 
-data, address = udp_server_socket.recvfrom(BUFFER_SIZE) #quando un client invia dei dati, vengono messi in data
-print(f"Messaggio ricevuto: {data.decode('utf8')} da {address}")
+data, client_address = udp_server_socket.recvfrom(BUFFER_SIZE) #quando un client invia dei dati, vengono messi in data
+print(f"Messaggio ricevuto: {data.decode('utf-8')} da {client_address}")
+
+udp_server_socket.sendto("Benvenuto nel server !!".encode("utf-8"), client_address)
+udp_server_socket.close()
